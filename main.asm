b85c22277e89bd97f34dc0896738ad4c8858840b
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-10-12 00:00:37 -0500

    ✨️ Adds setting fields

--- main.asm
+++ main.asm
@@ -3,10 +3,14 @@
 EMPTY equ 0
 OWNED_O equ 1
 OWNED_X equ 2
+TURN_O equ 0
+TURN_X equ 1
 
 section .data
-	inputMsg db "> ", 0x0
-	inputLen equ $ - inputMsg
+    inputStartMsg db "(", 0x0
+    inputStartLen equ $ - inputStartMsg
+	inputEndMsg db ") > ", 0x0
+	inputEndLen equ $ - inputEndMsg
 	endlMsg db 0xA, 0x0
 	endlLen equ $ - endlMsg
 	spaceMsg db "E", 0x0
@@ -26,6 +30,7 @@ section .data
 	      db EMPTY ; 6
           db EMPTY ; 7
           db EMPTY ; 8
+    turn db TURN_O
 
 section .bss
 	input resb 32
@@ -43,9 +48,33 @@ _start:
     loop:
 	mov rax, SYS_WRITE
 	mov rbx, STDOUT
-	mov rcx, inputMsg
-	mov rdx, inputLen
+	mov rcx, inputStartMsg
+	mov rdx, inputStartLen
 	int 0x80
+
+	cmp byte[turn], TURN_X
+	je turnX
+
+    mov rax, SYS_WRITE
+    mov rbx, STDOUT
+    mov rcx, oMsg
+    mov rdx, oLen
+    int 0x80
+    jmp endInputPrint
+
+    turnX:
+    mov rax, SYS_WRITE
+    mov rbx, STDOUT
+    mov rcx, xMsg
+    mov rdx, xLen
+    int 0x80
+
+    endInputPrint:
+    mov rax, SYS_WRITE
+    mov rbx, STDOUT
+    mov rcx, inputEndMsg
+    mov rdx, inputEndLen
+    int 0x80
 
 	mov rax, SYS_READ
 	mov rbx, STDIN
@@ -53,6 +82,77 @@ _start:
 	mov rdx, 32
 	int 0x80
 
+	mov r9, input
+	mov r10, 0
+
+    cmp byte[r9], 0x30
+    je set0
+	cmp byte[r9], 0x31
+	je set1
+	cmp byte[r9], 0x32
+	je set2
+	cmp byte[r9], 0x33
+	je set3
+	cmp byte[r9], 0x34
+	je set4
+	cmp byte[r9], 0x35
+	je set5
+	cmp byte[r9], 0x36
+	je set6
+	cmp byte[r9], 0x37
+	je set7
+	cmp byte[r9], 0x38
+	je set8
+
+	jmp exit
+
+	set0:
+	mov r10, 0
+	jmp endSet
+
+	set1:
+	mov r10, 1
+	jmp endSet
+
+	set2:
+	mov r10, 2
+	jmp endSet
+
+	set3:
+	mov r10, 3
+	jmp endSet
+
+	set4:
+    mov r10, 4
+    jmp endSet
+
+    set5:
+    mov r10, 5
+    jmp endSet
+
+    set6:
+    mov r10, 6
+    jmp endSet
+
+    set7:
+    mov r10, 7
+    jmp endSet
+
+    set8:
+    mov r10, 8
+    jmp endSet
+
+    endSet:
+	cmp byte[turn], TURN_X
+	je updateFieldX
+
+    mov byte[field+r10], OWNED_O
+    jmp endUpdateField
+
+    updateFieldX:
+    mov byte[field+r10], OWNED_X
+
+    endUpdateField:
 	display:
 	mov r8, 0
 
@@ -107,7 +207,6 @@ _start:
     mov rcx, endlMsg
     mov rdx, endlLen
     int 0x80
-    jmp displayEnd
 
     displayEnd:
     inc r8
@@ -119,6 +218,17 @@ _start:
     int 0x80
     cmp r8, 9
     jl displayLoop
+
+    cmp byte[turn], TURN_X
+    je setTurnO
+
+    mov byte[turn], TURN_X
+    jmp endSetTurn
+
+    setTurnO:
+    mov byte[turn], TURN_O
+
+    endSetTurn:
     jmp loop
 
 	exit:
